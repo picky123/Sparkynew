8ee3da2f71c3eaf593822cd6681d00dfd1fef3f3
picky123 <38593029+picky123@users.noreply.github.com>
2018-04-25 22:33:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -39,5 +39,20 @@ bot.on("message", async message => {
   if(commandfile) commandfile.run(bot,message,args);
 });
 
-
 bot.login('NDE0NzA2Njk4NjcxMDk1ODE5.DcHpFA.yai98SIEzBJUu-uCYNfKZxAMGBE')
+
+exports.run = async (bot, message, args) => {
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+        .setDescription("Bot Information")
+        .setColor("#15f153")
+        .setThumbnail(bicon)
+        .addField("Bot Name", bot.user.username)
+        .addField("Created On", bot.user.createdAt);
+
+    message.channel.send(botembed);
+}
+
+module.exports.help = {
+    name: "botinfo"
+}
